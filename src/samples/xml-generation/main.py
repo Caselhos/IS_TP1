09e9b658ad8b09dda96816668f1b2d54ee723f00
linux <goncalocaselhos@ipvc.pt>
2023-11-30 14:19:21 +0000

    remover comment

--- src/samples/xml-generation/main.py
+++ src/samples/xml-generation/main.py
@@ -1,6 +1,5 @@
 from csv_to_xml_converter import CSVtoXMLConverter
 
 if __name__ == "__main__":
-    #converter = CSVtoXMLConverter("/data/test.csv")
-    converter = CSVtoXMLConverter("test.csv")
+    converter = CSVtoXMLConverter("/data/test.csv")
     print(converter.to_xml_str())
